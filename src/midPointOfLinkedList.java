0421fa563c99c272d1ee0699736f159e59e79fe2
Amit Rana <123amitrana0123@gmail.com>
2023-03-19 13:07:19 +0530

    solved mergeSort using linkedList

--- src/midPointOfLinkedList.java
+++ src/midPointOfLinkedList.java
@@ -1,22 +1,21 @@
 public class midPointOfLinkedList {
-    public static int helperFunction(LinkedListNode<Integer> head){
+    public static LinkedListNode<Integer> helperFunction(LinkedListNode<Integer> head){
         // using fast and slow pointer technique
-        if(head == null || head.next == null) return 0;
+        if(head == null || head.next == null) return head;
         LinkedListNode<Integer> fast = head.next;
         LinkedListNode<Integer> slow = head;
         while(fast != null && fast.next != null){
             slow = slow.next;
             fast = fast.next.next;
         }
-        return slow.data;
+        return slow;
     }
     public static void main(String args[]){
         int linkedListArr[] = {1};
         basicFunctions method = new basicFunctions();
         LinkedListNode<Integer> head = new LinkedListNode<Integer>(null);
         head = method.createLinkedList(linkedListArr);
-
-        System.out.println(helperFunction(head));;
+        System.out.println(helperFunction(head));
 
 
     }
